fa6128638abc193c6bff3747b01ce7f9b777a963
HAADEER <hadeeressam21@gmail.com>
2022-08-15 13:45:43 +0200

    adding provider, edit in search bar, and adding some style

--- Component/Card.js
+++ Component/Card.js
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import {Text, ScrollView, Image, View, TouchableOpacity} from 'react-native';
-import moment from "moment";
+import React, {useState} from 'react';
+import {Text, ScrollView, Image, View, TouchableOpacity, RefreshControl} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import moment from "moment";
 
 export default function Card({data}) {
   const navigation = useNavigation()
+  
   return (
-    <ScrollView>
-      <View style={{flex: 1, alignItems: 'center'}}>
+    <ScrollView showsHorizontalScrollIndicator={false} style={{ backgroundColor:"#f1f1f3"}}>
+      <View style={{flex: 1, alignItems: 'center' , marginHorizontal:11}}>
         {
         data.map((element, index) => {
           return (
@@ -24,18 +25,20 @@ export default function Card({data}) {
               flexDirection:'row' ,
               width:'100%' ,
               padding:15 ,
-              borderBottomColor:"#666" ,
-              borderBottomWidth:1 ,
-              borderBottomRightRadius:16 ,
-              borderBottomLeftRadius:16
+              borderColor:"transparent" ,
+              borderWidth:1 ,
+              borderRadius:19,
+              marginVertical:11,
+              backgroundColor:"#fff"
               }}>
               <Image
                 source={{uri: `${element.urlToImage}`}}
-                style={{height: 160, width: 105 , marginRight:9}}
+                style={{height: 100, width: 105 , marginRight:9}}
               />
-              <View style={{flexShrink:1 }}>
-                <Text style={{fontSize: 19, fontWeight: 'bold' , color:"#000"}}>{element.source.name}</Text>
-                <Text style={{fontSize: 19 ,}}>{element.title}</Text>
+              <View style={{flexShrink:1 , paddingVertical:9}}>
+                <Text style={{fontSize: 17 , color:"#666"}}>{element.source.name}</Text>
+                <Text style={{fontSize:19 , fontWeight:'bold' , paddingEnd:7 , color:"#000"}} numberOfLines={2}>{element.title}</Text>
+                <Text style={{textAlign:'right' , marginTop:5}}>{moment(element.publishedAt).utc().format("MM-DD-YYYY")}</Text>
               </View>
             </TouchableOpacity>
           );
--- Component/Header.js
+++ Component/Header.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View,Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { searchContext } from "../Providers/SearchProvider";
 
 export default function Header({title}){
     const navigation = useNavigation()
+    const {setSearchInput} = useContext(searchContext)
+    const navigateToHome = () => {
+        setSearchInput("")
+        navigation.goBack()
+    }
     return(
         <View style={{height:60,display:'flex' , justifyContent:"center" , alignItems:"center" , backgroundColor:"#31087B"}}>
             {
-                title === "New App"
+                title === "All News"
                 ?
-                <Text style={{color:"#fff" , fontSize:23 , fontWeight:"bold"}}>{title}</Text>
+                <Text style={{color:"#fff" , fontSize:25 , fontWeight:"bold"}}>{title}</Text>
                 :
                 <View style={{justifyContent:'space-between' ,flexDirection:'row' , alignItems:'center' , margin:4}}>
-                <TouchableOpacity onPress={()=>navigation.goBack()} >
+                <TouchableOpacity onPress={()=>{navigateToHome()}} >
                     <MaterialIcons name="arrow-back" color='#fff' size={25}/>
                 </TouchableOpacity>
                 <View style={{flex:1}}>
